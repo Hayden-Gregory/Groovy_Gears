0bcc1e92496badc9827bb0f226dc9b5c60fd529a
Mort-The-Chicken <33301967+Mort-The-Chicken@users.noreply.github.com>
2018-01-10 16:24:27 -0500

    Initial Import

---
+++ Assets/Conductor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conductor : MonoBehaviour {
+
+    //Conductor
+    public float bpm = 180;
+    public float crotchet;
+    public float songposition;
+    public float offset = 0.2f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      //  songposition = (float)(AudioSettings.dspTime – dsptimesong) *song.pitch – offset;
+    }
+}
