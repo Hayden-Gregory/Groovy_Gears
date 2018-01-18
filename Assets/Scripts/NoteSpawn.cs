0674a8fe692418da204e07ccc8faa1a34bcbfd87
unknown <shamikraje@protonmail.ch>
2018-01-17 20:54:13 -0500

    Added Note spawning on key press

---
+++ Assets/Scripts/NoteSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteSpawn : MonoBehaviour
+{
+    public GameObject quarterNote;
+    private float x;
+    public Vector2 velocity;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.anyKeyDown)
+        {
+            GameObject quartNote = (GameObject)Instantiate(quarterNote, new Vector2(-2, 3), Quaternion.identity);
+            quartNote.GetComponent<Rigidbody2D>().velocity = velocity;
+        }
+    }
+}
