b8e7d7e08cd81db2d35d5badd253c94366cc0fdb
Mort-The-Chicken <33301967+Mort-The-Chicken@users.noreply.github.com>
2018-01-17 20:12:24 -0500

    Added a health script and a player prefab

---
+++ Assets/Script/Health.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    public int startingHealth = 100;
+    public int currentHealth;
+
+    bool damaged;
+    bool isDead;
+	// Use this for initialization
+	void Start () {
+        currentHealth = startingHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //If we took damage...
+        if (damaged)
+        {
+            //here we should set a death notification
+        }
+        else
+        {
+            //transition back to no damage
+        }
+
+        //reset the damaged flag
+        damaged = false;
+	}
+
+    public void TakeDamage (int amount)
+    {
+        //use this to set a flash/damage animation in our damaged method
+        damaged = true;
+      
+        // reduce player health by damage amount.
+        currentHealth -= amount;
+        
+        //check if we are dead.
+        if(currentHealth <= 0 && !isDead)
+        {
+            //our method deciding how we die yay
+            Death();
+        }
+    }
+}
